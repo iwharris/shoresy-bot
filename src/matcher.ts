602283096aab419ec156b3996de37937da2d9b86
Ian Harris <git@ianharris.io>
2022-02-23 17:12:43 -0700

    Clean up matchers

--- src/matcher.ts
+++ src/matcher.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import * as chirps from './chirps';
 
-const shorseySpellings = ['shoresy', 'shorsey'];
+const shoresySpellings = ['shoresy', 'shorsey'];
 
 export interface IMatchers<Context = any> {
     getMatch(context: Context): IMatcher | undefined;
@@ -28,21 +28,23 @@ export class Matchers implements IMatchers<CommentContext> {
 
     constructor() {
         const mentionsAny = (ctx: CommentContext, terms: string[]): boolean => terms.some((t) => ctx.body.includes(t));
+        const mentions = (ctx: CommentContext, term: string): boolean => mentionsAny(ctx, [term]);
         const mentionsAll = (ctx: CommentContext, terms: string[]): boolean => terms.every((t) => ctx.body.includes(t));
-        const mentionsShoresy = (ctx: CommentContext): boolean => mentionsAny(ctx, shorseySpellings);
+        const mentionsShoresy = (ctx: CommentContext): boolean => mentionsAny(ctx, shoresySpellings);
         const threadHasSecondCommenter = (ctx: CommentContext): boolean => !!ctx.name2;
 
+        // matchers are evaluated in order until a match is found
         // matchers go from specific to general
         this.matchers = [
-            new CommentMatcher((ctx) => mentionsAny(ctx, ['gretz']), chirps.gretz),
+            new CommentMatcher((ctx) => mentions(ctx, 'gretz'), chirps.gretz),
             new CommentMatcher(
                 (ctx) => mentionsAny(ctx, ['mister', 'mr']) && mentionsAny(ctx, ['hockey']),
                 chirps.howe
             ),
-            new CommentMatcher((ctx) => mentionsAny(ctx, ['happen']) && mentionsShoresy(ctx), chirps.happen),
-            new CommentMatcher((ctx) => mentionsAny(ctx, ['coming']) && mentionsShoresy(ctx), chirps.coming),
-            new CommentMatcher((ctx) => mentionsAny(ctx, ['line']) && mentionsShoresy(ctx), chirps.lines),
-            new CommentMatcher((ctx) => mentionsAny(ctx, ['ref']) && mentionsShoresy(ctx), chirps.ref),
+            new CommentMatcher((ctx) => (mentionsAll(ctx, ['what', 'gonna', 'happen'])) || (mentionsAny(ctx, ['happen']) && mentionsShoresy(ctx)), chirps.happen),
+            new CommentMatcher((ctx) => mentions(ctx, 'coming') && mentionsShoresy(ctx), chirps.coming),
+            new CommentMatcher((ctx) => mentions(ctx, 'line') && mentionsShoresy(ctx), chirps.lines),
+            new CommentMatcher((ctx) => mentions(ctx, 'ref') && mentionsShoresy(ctx), chirps.ref),
             new CommentMatcher(
                 (ctx) => mentionsAll(ctx, ['fuck', 'you']) && mentionsShoresy(ctx) && threadHasSecondCommenter(ctx),
                 [...chirps.chirps, ...chirps.chirps1, ...chirps.chirps2]
